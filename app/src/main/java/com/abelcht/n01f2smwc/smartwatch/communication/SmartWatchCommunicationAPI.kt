67ed18e34021e9d187606bcb375381ca7bac7281
AbelChT <abelcht@outlook.es>
2020-05-04 12:09:26 +0200

    first commit

---
+++ app/src/main/java/com/abelcht/n01f2smwc/smartwatch/communication/SmartWatchCommunicationAPI.kt
@@ -0,0 +1,22 @@
+package com.abelcht.n01f2smwc.smartwatch.communication
+
+object SmartWatchCommunicationAPI {
+    fun connectToSmartWatch(deviceMac: String): Boolean {
+        // TODO: Implement
+        return true
+    }
+
+    fun configureSmartWatch() {
+        // TODO: Implement
+    }
+
+    fun isConnectedToSmartWatch(): Boolean {
+        // TODO: Implement
+        return true
+    }
+
+    fun disconnectFromSmartWatch() {
+        // TODO: Implement
+    }
+
+}
---
+++ app/src/main/java/com/abelcht/n01f2smwc/smartwatch/config/DeviceInfo.kt
@@ -0,0 +1,6 @@
+package com.abelcht.n01f2smwc.smartwatch.config
+
+object DeviceInfo {
+    const val SmartWatchMac: String = ""
+    val SmartWatchConfigurationPackage: Array<Int> = arrayOf(0x00, 0x00)
+}
