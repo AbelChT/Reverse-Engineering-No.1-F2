90110586a28af47367282243dee64d3f3c8e2a21
AbelChT <abelcht@outlook.es>
2020-05-10 11:39:14 +0200

    testing ble2

--- app/src/androidTest/java/com/abelcht/n01f2smwc/SmartWatchCommunicationInstrumentedTest.kt
+++ app/src/androidTest/java/com/abelcht/n01f2smwc/SmartWatchCommunicationInstrumentedTest.kt
@@ -1,11 +1,15 @@
 package com.abelcht.n01f2smwc
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -47,11 +51,43 @@ class SmartWatchCommunicationInstrumentedTest {
             }
         }
 
-        val bluetoothLeScanner = bluetoothAdapter!!.bluetoothLeScanner
-        bluetoothLeScanner.startScan(scanCallback)
-        println("Sleep start")
+        // Discover devices
+        if (bluetoothAdapter!!.isDiscovering()) {
+            bluetoothAdapter!!.cancelDiscovery()
+        }
+
+
+
+        // Create a BroadcastReceiver for ACTION_FOUND.
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action: String = intent.action!!
+                when (action) {
+                    BluetoothDevice.ACTION_FOUND -> {
+                        // Discovery has found a device. Get the BluetoothDevice
+                        // object and its info from the Intent.
+                        val device: BluetoothDevice =
+                            intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                        val deviceName = device.name
+                        val deviceHardwareAddress = device.address // MAC address
+
+                        println("Found device: $deviceName $deviceHardwareAddress")
+                    }
+                }
+            }
+        }
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        appContext.registerReceiver(receiver, filter)
+        println("Start discovering")
+        bluetoothAdapter!!.startDiscovery()
+
         Thread.sleep(10000)
-        println("Sleep end")
-        bluetoothLeScanner.stopScan(scanCallback)
+        bluetoothAdapter!!.cancelDiscovery()
+
+        println("End discovering")
+
+        appContext.unregisterReceiver(receiver)
     }
 }
